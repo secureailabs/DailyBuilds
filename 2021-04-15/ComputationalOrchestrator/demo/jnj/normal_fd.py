3cc2ed2e86b9609d9cecc1ac2319646b74ed438c
JingweiZhangBoston <kukuthedoydoy@gmail.com>
2021-04-16 04:53:27 -0400

    update demo scripts

--- 2021-04-15/ComputationalOrchestrator/demo/jnj/normal_fd.py
+++ 2021-04-15/ComputationalOrchestrator/demo/jnj/normal_fd.py
@@ -5,10 +5,10 @@ import numpy as np
 data = 0
 xmin = 0
 xmax = 0
-col_num = 0
+col_id = 0
 
 with open(sys.argv[0], 'rb') as f:
-    col_num = pickle.load(f)
+    col_id = pickle.load(f)
 with open(sys.argv[1], 'rb') as f:
     xmin = pickle.load(f)
 with open(sys.argv[2], 'rb') as f:
@@ -16,9 +16,7 @@ with open(sys.argv[2], 'rb') as f:
 with open(sys.argv[3], 'rb') as f:
     data = pickle.load(f)
 
-col = data[:, col_num]
-for x in col:
-    x = (x-xmin)/(xmax-xmin)
+data[col_id] = (data[col_id]-xmin)/(xmax-xmin)
 
 with open(sys.argv[4], 'wb') as f:
-    pickle.dump(col, f)
+    pickle.dump(data, f)
